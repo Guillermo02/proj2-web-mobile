937e5add5a9e98dd3a3881c161cf0d4a383d3da3
Guillermo02 <62570122+Guillermo02@users.noreply.github.com>
2026-04-15 11:18:38 +0000

    Adição de sub páginas

---
+++ src/app/subPage/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link"; //Reload só dos elementos que mudaram da página
+
+export default function sobre(){
+    return (
+        <>
+            <h1>pagina 2</h1>
+        </>
+    );
+}
